57d6aa0b455a771d115574642ae7df0c612a64d0
LegendaryHori <shotakobayashi7@gmail.com>
2024-12-30 08:03:57 +0900

    wg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,7 +45,7 @@ export default function Home() {
     try {
       console.log("送信データ:", data);
 
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/process`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/process`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
